7b4fb49483434d338fbc11984badfe3160a50327
DESKTOP-F3AD4QO\citrix <nachiappan.kumarappan@citrix.com>
2020-08-18 00:42:14 +0530

    Clean up

--- examples/Titanium.Web.Proxy.Examples.Basic/ProxyTestController.cs
+++ examples/Titanium.Web.Proxy.Examples.Basic/ProxyTestController.cs
@@ -53,62 +53,18 @@ namespace Titanium.Web.Proxy.Examples.Basic
             proxyServer.BeforeRequest += onRequest;
             proxyServer.BeforeResponse += onResponse;
             proxyServer.AfterResponse += onAfterResponse;
-
             proxyServer.ServerCertificateValidationCallback += OnCertificateValidation;
             proxyServer.ClientCertificateSelectionCallback += OnCertificateSelection;
-
-            //proxyServer.EnableWinAuth = true;
-
             explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000);
-
-            // Fired when a CONNECT request is received
             explicitEndPoint.BeforeTunnelConnectRequest += onBeforeTunnelConnectRequest;
             explicitEndPoint.BeforeTunnelConnectResponse += onBeforeTunnelConnectResponse;
-
-            // An explicit endpoint is where the client knows about the existence of a proxy
-            // So client sends request in a proxy friendly manner
             proxyServer.AddEndPoint(explicitEndPoint);
             proxyServer.Start();
-
-            // Transparent endpoint is useful for reverse proxy (client is not aware of the existence of proxy)
-            // A transparent endpoint usually requires a network router port forwarding HTTP(S) packets or DNS
-            // to send data to this endPoint
-            //var transparentEndPoint = new TransparentProxyEndPoint(IPAddress.Any, 443, true)
-            //{ 
-            //    // Generic Certificate hostname to use
-            //    // When SNI is disabled by client
-            //    GenericCertificateName = "google.com"
-            //};
-
-            //proxyServer.AddEndPoint(transparentEndPoint);
-            //proxyServer.UpStreamHttpProxy = new ExternalProxy("localhost", 8888);
-            //proxyServer.UpStreamHttpsProxy = new ExternalProxy("localhost", 8888);
-
-            // SOCKS proxy
-            //proxyServer.UpStreamHttpProxy = new ExternalProxy("127.0.0.1", 1080)
-            //    { ProxyType = ExternalProxyType.Socks5, UserName = "User1", Password = "Pass" };
-            //proxyServer.UpStreamHttpsProxy = new ExternalProxy("127.0.0.1", 1080)
-            //    { ProxyType = ExternalProxyType.Socks5, UserName = "User1", Password = "Pass" };
-
-
-            //var socksEndPoint = new SocksProxyEndPoint(IPAddress.Any, 1080, true)
-            //{
-            //    // Generic Certificate hostname to use
-            //    // When SNI is disabled by client
-            //    GenericCertificateName = "google.com"
-            //};
-
-            //proxyServer.AddEndPoint(socksEndPoint);
-
             foreach (var endPoint in proxyServer.ProxyEndPoints)
             {
                 Console.WriteLine("Listening on '{0}' endpoint at Ip {1} and port: {2} ", endPoint.GetType().Name,
                     endPoint.IpAddress, endPoint.Port);
             }
-
-            // Only explicit proxies can be set as system proxy!
-            //proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
-            //proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
             if (RunTime.IsWindows)
             {
                 proxyServer.SetAsSystemProxy(explicitEndPoint, ProxyProtocolType.AllHttp);
@@ -128,38 +84,6 @@ namespace Titanium.Web.Proxy.Examples.Basic
             proxyServer.Stop();
 
             return logs;
-        }
-
-        private async Task<IExternalProxy> onGetCustomUpStreamProxyFunc(SessionEventArgsBase arg)
-        {
-            arg.GetState().PipelineInfo.AppendLine(nameof(onGetCustomUpStreamProxyFunc));
-
-            // this is just to show the functionality, provided values are junk
-            return new ExternalProxy
-            {
-                BypassLocalhost = false,
-                HostName = "127.0.0.9",
-                Port = 9090,
-                Password = "fake",
-                UserName = "fake",
-                UseDefaultCredentials = false
-            };
-        }
-
-        private async Task<IExternalProxy> onCustomUpStreamProxyFailureFunc(SessionEventArgsBase arg)
-        {
-            arg.GetState().PipelineInfo.AppendLine(nameof(onCustomUpStreamProxyFailureFunc));
-
-            // this is just to show the functionality, provided values are junk
-            return new ExternalProxy
-            {
-                BypassLocalhost = false,
-                HostName = "127.0.0.10",
-                Port = 9191,
-                Password = "fake2",
-                UserName = "fake2",
-                UseDefaultCredentials = false
-            };
         }
 
         private async Task onBeforeTunnelConnectRequest(object sender, TunnelConnectSessionEventArgs e)
@@ -228,7 +152,6 @@ namespace Titanium.Web.Proxy.Examples.Basic
             return Task.CompletedTask;
         }
 
-        // intercept & cancel redirect or update requests
         private async Task onRequest(object sender, SessionEventArgs e)
         {
             e.GetState().PipelineInfo.AppendLine(nameof(onRequest) + ":" + e.HttpClient.Request.RequestUri);
@@ -247,25 +170,6 @@ namespace Titanium.Web.Proxy.Examples.Basic
                 await writeToConsole(e.HttpClient.Request.Url);
                 await writeToConsole("ProcessID " + e.HttpClient.ProcessId.Value.ToString());
             }
-
-
-            // store it in the UserData property
-            // It can be a simple integer, Guid, or any type
-            //e.UserData = new CustomUserData()
-            //{
-            //    RequestHeaders = e.HttpClient.Request.Headers,
-            //    RequestBody = e.HttpClient.Request.HasBody ? e.HttpClient.Request.Body:null,
-            //    RequestBodyString = e.HttpClient.Request.HasBody? e.HttpClient.Request.BodyString:null
-            //};
-
-            ////This sample shows how to get the multipart form data headers
-            //if (e.HttpClient.Request.Host == "mail.yahoo.com" && e.HttpClient.Request.IsMultipartFormData)
-            //{
-            //    e.MultipartRequestPartSent += MultipartRequestPartSent;
-            //}
-
-
-
         }
 
         private bool IsValidHost(string url)
@@ -273,18 +177,7 @@ namespace Titanium.Web.Proxy.Examples.Basic
             return hostNames.Any(x => url.Contains(x));
         }
 
-        // Modify response
-        private async Task multipartRequestPartSent(object sender, MultipartRequestPartSentEventArgs e)
-        {
-            e.GetState().PipelineInfo.AppendLine(nameof(multipartRequestPartSent));
 
-            var session = (SessionEventArgs)sender;
-            //TBD await writeToConsole("Multipart form data headers:");
-            foreach (var header in e.Headers)
-            {
-               //TBD await writeToConsole(header.ToString());
-            }
-        }
 
         private async Task onResponse(object sender, SessionEventArgs e)
         {
@@ -305,86 +198,30 @@ namespace Titanium.Web.Proxy.Examples.Basic
                 await writeToConsole("response " + e.HttpClient.Request.Url);
                 await writeToConsole("response 1" + ext);
                 await writeToConsole("ProcessID " + e.HttpClient.ProcessId.Value.ToString());
-
             }
-            // access user data set in request to do something with it
-            //var userData = e.HttpClient.UserData as CustomUserData;
-
-            //if (ext == ".gif" || ext == ".png" || ext == ".jpg")
-            //{ 
-            //    byte[] btBody = Encoding.UTF8.GetBytes("<!DOCTYPE html>" +
-            //                                           "<html><body><h1>" +
-            //                                           "Image is blocked" +
-            //                                           "</h1>" +
-            //                                           "<p>Blocked by Titanium</p>" +
-            //                                           "</body>" +
-            //                                           "</html>");
-
-            //    var response = new OkResponse(btBody);
-            //    response.HttpVersion = e.HttpClient.Request.HttpVersion;
-
-            //    e.Respond(response);
-            //    e.TerminateServerConnection();
-            //} 
-
-            //// print out process id of current session
-            ////WriteToConsole($"PID: {e.HttpClient.ProcessId.Value}");
-
-            ////if (!e.ProxySession.Request.Host.Equals("medeczane.sgk.gov.tr")) return;
-            //if (e.HttpClient.Request.Method == "GET" || e.HttpClient.Request.Method == "POST")
-            //{ 
-            //    if (e.HttpClient.Response.StatusCode == (int)HttpStatusCode.OK)
-            //    {
-            //        if (e.HttpClient.Response.ContentType != null && e.HttpClient.Response.ContentType.Trim().ToLower().Contains("text/html"))
-            //        {
-            //            var bodyBytes = await e.GetResponseBody();
-            //            await e.SetResponseBody(bodyBytes);
-
-            //            string body = await e.GetResponseBodyAsString();
-            //            await e.SetResponseBodyString(body);
-            //        }
-            //    }
-            //} 
         }
 
         private async Task onAfterResponse(object sender, SessionEventArgs e)
         {
-            
             if (IsValidHost(e.HttpClient.Request.Url))
             {
                 await writeToConsole($"Pipelineinfo: {e.GetState().PipelineInfo}", ConsoleColor.Yellow);
             }
         }
 
-        /// <summary>
-        ///     Allows overriding default certificate validation logic
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         public Task OnCertificateValidation(object sender, CertificateValidationEventArgs e)
         {
             e.GetState().PipelineInfo.AppendLine(nameof(OnCertificateValidation));
-
-            // set IsValid to true/false based on Certificate Errors
             if (e.SslPolicyErrors == SslPolicyErrors.None)
             {
                 e.IsValid = true;
             }
-
             return Task.CompletedTask;
         }
 
-        /// <summary>
-        ///     Allows overriding default client certificate selection logic during mutual authentication
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         public Task OnCertificateSelection(object sender, CertificateSelectionEventArgs e)
         {
             e.GetState().PipelineInfo.AppendLine(nameof(OnCertificateSelection));
-
-            // set e.clientCertificate to override
-
             return Task.CompletedTask;
         }
 
