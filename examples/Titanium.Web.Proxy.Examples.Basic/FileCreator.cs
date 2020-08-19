7baf47074f3a3d49eb81e59eac5768d06fefd5f1
DESKTOP-F3AD4QO\citrix <nachiappan.kumarappan@citrix.com>
2020-08-19 22:41:24 +0530

    some how only printing one log

---
+++ examples/Titanium.Web.Proxy.Examples.Basic/FileCreator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Titanium.Web.Proxy.Examples.Basic
+{
+    public class FileCreator
+    {
+        private static Random random = new Random();
+
+        public static async Task CreateFile(String path, int size)
+        {
+            if (File.Exists(path)) File.Delete(path);
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                int count = size / 1000;
+                if (count == 0)
+                    count++;
+                for (int i = 0; i < count; i++)
+                {
+                    await sw.WriteAsync((string)RandomString(1000));
+                }
+
+                await sw.FlushAsync();
+            }
+            
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
--- examples/Titanium.Web.Proxy.Examples.Basic/Program.cs
+++ examples/Titanium.Web.Proxy.Examples.Basic/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Titanium.Web.Proxy.Examples.Basic.Performance;
@@ -10,80 +8,48 @@ namespace Titanium.Web.Proxy.Examples.Basic
 {
     public class Program
     {
-        private static string outputFileName1 = "proxy.txt";
-        private static string workingDirectory1 = @"C:\Data\";
-        private static int KB = 1024;
-        private static int MB = KB * KB;
+        private static int OneKb = 1024;
+        private static int OneMb = OneKb * OneKb;
 
         public static void Main(string[] args)
         {
 
-            //NetworkMonitor nm = new NetworkMonitor();
-            //nm.Monitor(300).GetAwaiter().GetResult();
+            Test().GetAwaiter().GetResult();
+            
+        }
 
-            //Code to intercept the network traffic.
- 
-            var result = new PerformanceProbe("sharefile", "szchanaa").GetFileLogs(60, 3, (x) => { return true; }, (x) => { return true; }).GetAwaiter().GetResult();
+
+        private static async Task Test()
+        {
+            var performanceProbe = new PerformanceProbe("sharefile", "szchanaa");
+
+
+            Console.WriteLine("Do preparation");
+            var pt = performanceProbe.GetFileLogs(300, 1, StartCounter, EndCounter);
+            Console.WriteLine("Do the test");
+            var result = await pt;
             Console.WriteLine(result);
-
-            //Code to generate a random file
-            //CreateFile(workingDirectory1+"somfile.txt", 4 * MB).GetAwaiter().GetResult();
+            Console.WriteLine("Do Clean up");
         }
 
-        private static void OnRequest(String url, long size)
+        private static bool StartCounter(NetworkAction a)
         {
-            Console.WriteLine(DateTime.Now.ToLongTimeString() + "\t" + size + "\t" + url);
+            if (a.Type != NetworkActionType.Request)
+                return false;
+            if (!a.Url.Contains("Upload2"))
+                return false;
+            return true;
         }
 
-        private static void OnResponse(String url, String body, int statusCode, String method, long size)
+
+        private static bool EndCounter(NetworkAction a)
         {
-
-            if (url.Contains("upload-threaded-3"))
-            {
-                Console.WriteLine(DateTime.Now.ToLongTimeString() + " " + url);
-            }
-
-            if (url.Contains("Upload2"))
-            {
-                Console.WriteLine(DateTime.Now.ToLongTimeString() + "response 1 " + url);
-                Console.WriteLine(DateTime.Now.ToLongTimeString() + "response 2 " + body);
-                Console.WriteLine(DateTime.Now.ToLongTimeString() + "response 3 " + statusCode);
-                Console.WriteLine(DateTime.Now.ToLongTimeString() + "response 4 " + method);
-                Console.WriteLine(DateTime.Now.ToLongTimeString() + "response 5 " + size);
-
-            }
+            if (a.Type != NetworkActionType.Request)
+                return false;
+            if (!a.Url.Contains("upload-threaded-3.aspx"))
+                return false;
+            return true;
         }
 
-        private static Random random = new Random();
-
-        public static async Task CreateFile(String file, int size)
-        {
-            string path = file;
-            // This text is added only once to the file.
-            if (!File.Exists(path))
-            {
-                // Create a file to write to.
-                using (StreamWriter sw = File.CreateText(path))
-                {
-                    int count = size / 1000;
-                    if (count == 0)
-                        count++;
-                    for (int i = 0; i < count; i++)
-                    {
-                        await sw.WriteAsync(RandomString(1000));
-                    }
-
-                    await sw.FlushAsync();
-                }
-            }
-        }
-
-
-        public static string RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
     }
 }
