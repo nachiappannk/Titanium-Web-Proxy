6e21a4d37e9c82daca5362db7c17af7345b5d2ce
DESKTOP-F3AD4QO\citrix <nachiappan.kumarappan@citrix.com>
2020-08-20 00:12:54 +0530

    Done... And Dusted... lets take rest

--- examples/Titanium.Web.Proxy.Examples.Basic/Performance/SessionLog.cs
+++ examples/Titanium.Web.Proxy.Examples.Basic/Performance/SessionLog.cs
@@ -19,10 +19,14 @@ namespace Titanium.Web.Proxy.Examples.Basic.Performance
 
         public override string ToString()
         {
+            var time = "";
+            if (!IsCancelled)
+                time = (EndTime - StarTime).TotalSeconds.ToString();
+
             StringBuilder builder = new StringBuilder();
-            builder.AppendLine($"Total Time (s) is \t{(EndTime - StarTime).TotalSeconds}");
+            builder.AppendLine($"Total Time (s) is \t{time}");
             builder.AppendLine($"Start Time is \t{StarTime.ToLongTimeString()}");
-            builder.AppendLine($"End Time (s) is \t{EndTime.ToLongTimeString()}");
+            builder.AppendLine($"End Time is \t{EndTime.ToLongTimeString()}");
             builder.AppendLine($"Is Cancelled is \t{IsCancelled}");
             foreach (var n in Actions)
             {
